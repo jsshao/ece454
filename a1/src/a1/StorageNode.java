4a5da0463e248a29cb202f4be2cf63e4c0df31cb
Jason Shao <jsshao@uwaterloo.ca>
2016-05-24 14:51:48 -0400

    Implement simple one-threaded server

--- a1/src/a1/StorageNode.java
+++ a1/src/a1/StorageNode.java
@@ -4,27 +4,49 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
 
-public class StorageNode {
-  public static void main(String [] args) throws Exception {
-      if (args.length != 2) {
-	  System.err.println("Usage: java a1.StorageNode config_file node_num");
-	  System.exit(-1);
-      }
-      BufferedReader br = new BufferedReader(new FileReader(args[0]));
-      HashMap<Integer, String> hosts = new HashMap<Integer, String>();
-      HashMap<Integer, Integer> ports  = new HashMap<Integer, Integer>();
-      String line;
-      int i = 0;
-      while ((line = br.readLine()) != null) {
-	  String[] parts = line.split(" ");
-	  hosts.put(i, parts[0]);
-	  ports.put(i, Integer.parseInt(parts[1]));
-	  i++;
-      }
-      int myNum = Integer.parseInt(args[1]);
-      System.out.println("My host and port: " + hosts.get(myNum) + ":" + ports.get(myNum));
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.TException;
+import org.apache.thrift.TProcessorFactory;
 
-      // Launch a Thrift server here
-      throw new Error("This code needs more work!");
-  }
+public class StorageNode {
+    public static void main(String [] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: java a1.StorageNode config_file node_num");
+            System.exit(-1);
+        }
+
+        BufferedReader br = new BufferedReader(new FileReader(args[0]));
+        HashMap<Integer, String> hosts = new HashMap<Integer, String>();
+        HashMap<Integer, Integer> ports  = new HashMap<Integer, Integer>();
+        String line;
+        int i = 0;
+        while ((line = br.readLine()) != null) {
+            String[] parts = line.split(" ");
+            hosts.put(i, parts[0]);
+            ports.put(i, Integer.parseInt(parts[1]));
+            i++;
+        }
+        int myNum = Integer.parseInt(args[1]);
+        System.out.println("My host and port: " + hosts.get(myNum) + ":" + ports.get(myNum));
+
+        // Launch a Thrift server here
+        try {
+            KeyValueService.Processor processor = 
+                new KeyValueService.Processor(new KeyValueHandler());
+            TServerSocket socket = new TServerSocket(ports.get(myNum));
+            TSimpleServer.Args sargs = new TSimpleServer.Args(socket);
+            sargs.protocolFactory(new TBinaryProtocol.Factory());
+            sargs.transportFactory(new TFramedTransport.Factory());
+            sargs.processorFactory(new TProcessorFactory(processor));
+            TServer server = new TSimpleServer(sargs);
+            server.serve();
+        } catch (TException x) {
+            // How to propagate?
+            throw x;
+        }
+    }
 }
