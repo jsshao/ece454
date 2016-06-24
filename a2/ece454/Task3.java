738e2a3fd5c74f53541d673d220c4e6939f14ae3
Jason Shao <jsshao@uwaterloo.ca>
2016-06-23 21:51:59 -0400

    Task3 combiner

--- a2/ece454/Task3.java
+++ a2/ece454/Task3.java
@@ -18,10 +18,10 @@ import org.apache.commons.lang.StringUtils;
 
 public class Task3 {
     public static class TokenizerMapper 
-             extends Mapper<Object, Text, IntWritable, IntWritable>{
+             extends Mapper<Object, Text, IntWritable, Text>{
 
         private IntWritable user  = new IntWritable();
-        private IntWritable rating  = new IntWritable();
+        private Text output = new Text();
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             String[] tokens = value.toString().split(",");
@@ -29,24 +29,44 @@ public class Task3 {
             for (int i = 1; i < tokens.length; i++) {
                 if (tokens[i].length() > 0) {
                     user.set(i);
-                    rating.set(Integer.parseInt(tokens[i]));
-                    context.write(user, rating);
+                    output.set(tokens[i] + ",1");
+                    context.write(user, output);
                 }
             }
         }
     }
 
+    public static class AvgCombiner
+            extends Reducer<IntWritable, Text, IntWritable, Text> {
+
+            private Text output = new Text();
+
+            public void reduce(IntWritable key, Iterable<Text> values,
+                    Context context) throws IOException, InterruptedException {
+                long sum = 0L;
+                long length = 0L;
+                for(Text value: values) {
+                    String[] tokens = value.toString().split(",", -1);
+                    sum += Integer.parseInt(tokens[0]);
+                    length += Integer.parseInt(tokens[1]);
+                }
+                output.set(Long.toString(sum) + "," + Long.toString(length));
+                context.write(key, output);
+            }
+    }
+
     public static class DoubleAvgReducer
-            extends Reducer<IntWritable, IntWritable, IntWritable, Text> {
+            extends Reducer<IntWritable, Text, IntWritable, Text> {
             private Text avg = new Text();
 
-            public void reduce(IntWritable key, Iterable<IntWritable> values,
+            public void reduce(IntWritable key, Iterable<Text> values,
                     Context context) throws IOException, InterruptedException {
                 Double sum = 0.0;
                 long length = 0;
-                for(IntWritable value: values) {
-                    sum += value.get();
-                    length += 1;
+                for(Text value: values) {
+                    String[] tokens = value.toString().split(",", -1);
+                    sum += Integer.parseInt(tokens[0]);
+                    length += Integer.parseInt(tokens[1]);
                 }
                 avg.set(String.format("%1.1f", sum/length));
                 context.write(key, avg);
@@ -64,10 +84,11 @@ public class Task3 {
         Job job = new Job(conf, "Rating Count");
         job.setJarByClass(Task3.class);
         job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(AvgCombiner.class);
         job.setReducerClass(DoubleAvgReducer.class);
         //job.setNumReduceTasks(1);
         job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(IntWritable.class);
+        job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(Text.class);
         TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
