681937c121ace12f65f9adaf40d7574a44df01e7
Jason Shao <jasonshao8@gmail.com>
2016-06-28 00:12:50 -0400

    playing around with DFS

--- a2b/ece454/CC.java
+++ a2b/ece454/CC.java
@@ -20,6 +20,7 @@ public class CC {
         // read graph from input file
         List<Edge> edges = new ArrayList<>();
         Set<Integer> vertices = new HashSet<>();
+        HashMap<Integer, ArrayList<Integer>> adj = new HashMap<Integer, ArrayList<Integer>>();
         FileInputStream fis = new FileInputStream(args[1]);
         BufferedReader br = new BufferedReader(new InputStreamReader(fis));
         String line = null;
@@ -33,13 +34,26 @@ public class CC {
             edges.add(e);
             vertices.add(v1);
             vertices.add(v2);
+            /*
+            if (!adj.containsKey(v1)) {
+                adj.put(v1, new ArrayList<Integer>());
+            }
+            adj.get(v1).add(v2);
+
+            if (!adj.containsKey(v2)) {
+                adj.put(v2, new ArrayList<Integer>());
+            }
+            adj.get(v2).add(v1);
+            */
         }
         br.close();
+        System.out.println("starting");
 
         // start worker threads
         Thread[] threads = new Thread[numThreads];
+        PrintWriter pw = new PrintWriter(new FileWriter(args[2]));
         for (int t = 0; t < numThreads; t++) {
-            threads[t] = new Thread(new MyRunnable());
+            threads[t] = new Thread(new MyRunnable(pw, vertices, adj));
             threads[t].start();
         }
 
@@ -49,16 +63,42 @@ public class CC {
         }
 
         // generate output
+        /*
         PrintWriter pw = new PrintWriter(new FileWriter(args[2]));
         for (int i: vertices) {
             pw.println(i + " " + "<replace with component label>");
         }
+        */
         pw.close();
         }
 
         static class MyRunnable implements Runnable {
+            PrintWriter pw;
+            Set<Integer> vertices;
+            HashMap<Integer, ArrayList<Integer>> adj;
+            HashSet<Integer> seen = new HashSet<Integer>();
+
+            public MyRunnable(PrintWriter p, Set<Integer> v, HashMap<Integer, ArrayList<Integer>> a) {
+                pw = p;
+                vertices = v;
+                adj = a;
+            }
+
             public void run() {
-                System.out.println("Hello World");
+                for (int v: vertices) {
+                    dfs(v, v);
+                }
+            }
+
+            public void dfs(int v, int label) {
+                if (seen.contains(v)) {
+                    return;
+                }
+                pw.printf("%d %d\n", v, label);
+                seen.add(v);
+                for (int v2: adj.get(v)) {
+                    dfs(v2, label);
+                }
             }
         }
 }
